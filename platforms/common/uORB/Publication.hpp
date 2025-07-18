0a682fccb57f5bce72020abb214b39abe05e579d
Parkhb1106 <stingyrain@gmail.com>
2025-07-18 14:11:13 +0900

    correct PublicationBase, Publication

--- platforms/common/uORB/Publication.hpp
+++ platforms/common/uORB/Publication.hpp
@@ -60,7 +60,12 @@ public:
 
 protected:
 
-	PublicationBase(ORB_ID id) : _orb_id(id) {}
+	PublicationBase(ORB_ID id) : _orb_id(id), _module_id(0) {}
+
+	// correction start
+        PublicationBase(ORB_ID id, uint _publisher_id) : _orb_id(id), _module_id(_publisher_id) {}
+        PublicationBase(uint _publisher_id) : _orb_id(ORB_ID::INVALID), _module_id(_publisher_id) {}
+        // corretion end
 
 	~PublicationBase()
 	{
@@ -74,6 +79,7 @@ protected:
 
 	orb_advert_t _handle{nullptr};
 	const ORB_ID _orb_id;
+	const uint _module_id;
 };
 
 /**
@@ -91,6 +97,12 @@ public:
 	 */
 	Publication(ORB_ID id) : PublicationBase(id) {}
 	Publication(const orb_metadata *meta) : PublicationBase(static_cast<ORB_ID>(meta->o_id)) {}
+
+	// correction start
+	Publication(ORB_ID id, uint _publisher_id) : PublicationBase(id, _publisher_id) {}
+	Publication(const orb_metadata *meta, uint _publisher_id) : PublicationBase(static_cast<ORB_ID>(meta->o_id), _publisher_id) {}
+	Publication(uint _publisher_id) : PublicationBase(_publisher_id) {}
+	// corretion end
 
 	bool advertise()
 	{
@@ -110,8 +122,15 @@ public:
 		if (!advertised()) {
 			advertise();
 		}
-
-		return (Manager::orb_publish(get_topic(), _handle, &data) == PX4_OK);
+		
+		if (_module_id != 0){
+			T copy_for_pub = data;
+			copy_for_pub.publisher_id = _module_id;
+			return (Manager::orb_publish(get_topic(), _handle, &copy_for_pub) == PX4_OK);
+		}
+		else{
+			return (Manager::orb_publish(get_topic(), _handle, &data) == PX4_OK);
+		}
 	}
 };
 
@@ -127,7 +146,7 @@ public:
 	 *
 	 * @param meta The uORB metadata (usually from the ORB_ID() macro) for the topic.
 	 */
-	PublicationData(ORB_ID id) : Publication<T>(id) {}
+	PublicationData(ORB_ID id) : Publication<T>(id) {}	
 	PublicationData(const orb_metadata *meta) : Publication<T>(meta) {}
 
 	T	&get() { return _data; }
